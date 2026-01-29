5feb35ae8c54cf25b97c1949fb4679d92895006e
ammys <ammyvega16@gmail.com>
2026-01-29 01:01:30 -0500

    Arreglos

--- sketch.js
+++ sketch.js
@@ -13,15 +13,22 @@ let sx = 0,
 let alphaFiltro = 0;
 let mensajeGuardado = 0;
 
+let globos = [];
+let imgsGlobos = [];
+
 function preload() {
   faceMesh = ml5.faceMesh(options);
   mal = loadSound("incorrecto.mp3");
   bien = loadSound("correcto.mp3");
   caraImg = loadImage("cara.png");
+
+  imgsGlobos.push(loadImage("globo_rojo.png"));
+  imgsGlobos.push(loadImage("globo_azul.png"));
+  imgsGlobos.push(loadImage("globo_amarillo.png"));
 }
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(800, 600);
 
   video = createCapture(VIDEO);
   video.size(640, 480);
@@ -41,6 +48,7 @@ function draw() {
   strokeWeight(3);
   rect(8, 8, width - 16, height - 16, 20);
 
+  // --- FaceMesh ---
   if (faces.length > 0) {
     let face = faces[0];
     let k = face.keypoints;
@@ -62,25 +70,34 @@ function draw() {
     }
   }
 
+  for (let g of globos) {
+    g.update();
+    g.draw();
+  }
+
   dibujarUI();
 }
 
+// ------------------ FILTRO ------------------
 function dibujarCara(face) {
   let k = face.keypoints;
+
+  let scaleX = width / video.width;
+  let scaleY = height / video.height;
 
   let izquierda = k[234];
   let derecha = k[454];
   let frente = k[10];
   let barbilla = k[152];
 
-  let x = (izquierda.x + derecha.x) / 2;
-  let y = (frente.y + barbilla.y) / 2;
+  let x = ((izquierda.x + derecha.x) / 2) * scaleX;
+  let y = ((frente.y + barbilla.y) / 2) * scaleY;
 
   sx = lerp(sx, x, 0.3);
   sy = lerp(sy, y, 0.3);
 
-  let w = dist(izquierda.x, izquierda.y, derecha.x, derecha.y);
-  let h = dist(frente.x, frente.y, barbilla.x, barbilla.y);
+  let w = dist(izquierda.x, izquierda.y, derecha.x, derecha.y) * scaleX;
+  let h = dist(frente.x, frente.y, barbilla.x, barbilla.y) * scaleY;
 
   let ojoIzq = k[33];
   let ojoDer = k[263];
@@ -92,7 +109,7 @@ function dibujarCara(face) {
 
   noStroke();
   fill(255, 80);
-  ellipse(0, 0, w * 1.1, h * 1.1);
+  ellipse(0, 0, w * 1.2, h * 1.2);
 
   tint(255, alphaFiltro);
   image(caraImg, 0, 0, w, h);
@@ -101,10 +118,40 @@ function dibujarCara(face) {
   pop();
 }
 
+// ------------------ GLOBOS ------------------
+class Globo {
+  constructor(img) {
+    this.img = img;
+    this.x = random(100, width - 100);
+    this.y = random(100, height - 100);
+    this.vel = 1;
+    this.tamaño = 120;
+  }
+
+  update() {
+    this.y -= this.vel;
+  }
+
+  draw() {
+    if (this.img) {
+      imageMode(CENTER);
+      image(this.img, this.x, this.y, this.tamaño, this.tamaño * 1.2);
+      imageMode(CORNER);
+    }
+  }
+}
+
+// ------------------ ESTADOS ------------------
 function activarFiltro() {
   if (!filtro) {
     bien.play();
     mal.stop();
+
+    globos = [];
+    for (let i = 0; i < 3; i++) {
+      let img = random(imgsGlobos);
+      globos.push(new Globo(img));
+    }
   }
   filtro = true;
 }
@@ -115,6 +162,7 @@ function desactivarFiltro() {
     bien.stop();
   }
   filtro = false;
+  globos = [];
 }
 
 function dibujarUI() {
@@ -135,6 +183,7 @@ function dibujarUI() {
   } else {
     text("Haz algo para activar el filtro", width / 2, height - 20);
   }
+
   if (mensajeGuardado > 0) {
     fill(0, 255, 160);
     text("✔ Foto guardada", width / 2, 30);
